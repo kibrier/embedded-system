0f79a86f2b9779510ea938dd194d795e439725c8
kibrier <145916158+kibrier@users.noreply.github.com>
2024-09-24 09:20:11 +0800

    Update test_receive.py

--- test_receive.py
+++ test_receive.py
@@ -1,25 +1,23 @@
-# #normal python
-# 
 import serial
-# import RPi.GPIO as GPIO
-# import time
-# 
-# led_pin = 13
-ser = serial.Serial(port='COM9',baudrate=115200, timeout=1)
-# 
-# GPIO.setwarnings(False)
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(led_pin, GPIO.OUT)
-# GPIO.output(led_pin, GPIO.LOW)
-# 
-# pwm = GPIO.pwm(led_pin, 1000)
-# pwm.start(0)
-# 
+import RPi.GPIO as GPIO
+import time
+
+led_pin = 13
+ser = serial.Serial(port='/dev/ttyACM0',baudrate=115200, timeout=1)
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(led_pin, GPIO.OUT)
+GPIO.output(led_pin, GPIO.LOW)
+
+pwm = GPIO.PWM(led_pin, 1000)
+pwm.start(0)
+
 def map(x, in_min, in_max, out_min, out_max):
     return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
-# 
+
 while True:
     if ser.readline():
         pot_value = int(ser.readline().decode('ascii'))
         print(pot_value, map(pot_value, 0, 1023, 0, 100))
-#         pwm.ChangeDutyCycle(map(pot_value, 0, 1023, 0, 100))
+        pwm.ChangeDutyCycle(map(pot_value, 0, 1023, 0, 100))
