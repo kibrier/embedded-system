5fa69d41857e36097a29b7b5da1c9cb5aa9ed33a
kibrier <145916158+kibrier@users.noreply.github.com>
2025-01-02 22:03:34 +0800

    Create ADS1x15.py

---
+++ lab 10/ADS1x15.py
@@ -0,0 +1,193 @@
+# Copyright (c) 2016 Adafruit Industries
+# Author: Tony DiCola
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import time
+
+# Register and other configuration values:
+ADS1x15_DEFAULT_ADDRESS        = 0x48
+ADS1x15_POINTER_CONVERSION     = 0x00
+ADS1x15_POINTER_CONFIG         = 0x01
+ADS1x15_POINTER_LOW_THRESHOLD  = 0x02
+ADS1x15_POINTER_HIGH_THRESHOLD = 0x03
+ADS1x15_CONFIG_OS_SINGLE       = 0x8000
+ADS1x15_CONFIG_MUX_OFFSET      = 12
+# Maping of gain values to config register values.
+ADS1x15_CONFIG_GAIN = {
+    2/3: 0x0000,
+    1:   0x0200,
+    2:   0x0400,
+    4:   0x0600,
+    8:   0x0800,
+    16:  0x0A00
+}
+ADS1x15_CONFIG_MODE_CONTINUOUS  = 0x0000
+ADS1x15_CONFIG_MODE_SINGLE      = 0x0100
+# Mapping of data/sample rate to config register values for ADS1015 (faster).
+ADS1015_CONFIG_DR = {
+    128:   0x0000,
+    250:   0x0020,
+    490:   0x0040,
+    920:   0x0060,
+    1600:  0x0080,
+    2400:  0x00A0,
+    3300:  0x00C0
+}
+# Mapping of data/sample rate to config register values for ADS1115 (slower).
+ADS1115_CONFIG_DR = {
+    8:    0x0000,
+    16:   0x0020,
+    32:   0x0040,
+    64:   0x0060,
+    128:  0x0080,
+    250:  0x00A0,
+    475:  0x00C0,
+    860:  0x00E0
+}
+ADS1x15_CONFIG_COMP_WINDOW      = 0x0010
+ADS1x15_CONFIG_COMP_ACTIVE_HIGH = 0x0008
+ADS1x15_CONFIG_COMP_LATCHING    = 0x0004
+ADS1x15_CONFIG_COMP_QUE = {
+    1: 0x0000,
+    2: 0x0001,
+    4: 0x0002
+}
+ADS1x15_CONFIG_COMP_QUE_DISABLE = 0x0003
+
+
+class ADS1x15(object):
+    """Base functionality for ADS1x15 analog to digital converters."""
+
+    def __init__(self, address=ADS1x15_DEFAULT_ADDRESS, i2c=None, **kwargs):
+        if i2c is None:
+            import Adafruit_GPIO.I2C as I2C
+            i2c = I2C
+        self._device = i2c.get_i2c_device(address, **kwargs)
+
+    def cleanup(self):
+        """Release the I2C resources and reset the device state."""
+        try:
+            # Resetting the I2C device state or releasing pins if required
+            self._device.writeList(ADS1x15_POINTER_CONFIG, [0x00, 0x00])
+        except Exception as e:
+            print(f"Warning: Cleanup failed: {e}")
+        finally:
+            self._device = None
+
+    def _data_rate_default(self):
+        """Retrieve the default data rate for this ADC (in samples per second).
+        Should be implemented by subclasses.
+        """
+        raise NotImplementedError('Subclasses must implement _data_rate_default!')
+
+    def _data_rate_config(self, data_rate):
+        """Subclasses should override this function and return a 16-bit value
+        that can be OR'ed with the config register to set the specified
+        data rate.  If a value of None is specified then a default data_rate
+        setting should be returned.  If an invalid or unsupported data_rate is
+        provided then an exception should be thrown.
+        """
+        raise NotImplementedError('Subclass must implement _data_rate_config function!')
+
+    def _conversion_value(self, low, high):
+        """Subclasses should override this function that takes the low and high
+        byte of a conversion result and returns a signed integer value.
+        """
+        raise NotImplementedError('Subclass must implement _conversion_value function!')
+
+    def _read(self, mux, gain, data_rate, mode):
+        """Perform an ADC read with the provided mux, gain, data_rate, and mode
+        values.  Returns the signed integer result of the read.
+        """
+        config = ADS1x15_CONFIG_OS_SINGLE  # Go out of power-down mode for conversion.
+        # Specify mux value.
+        config |= (mux & 0x07) << ADS1x15_CONFIG_MUX_OFFSET
+        # Validate the passed in gain and then set it in the config.
+        if gain not in ADS1x15_CONFIG_GAIN:
+            raise ValueError('Gain must be one of: 2/3, 1, 2, 4, 8, 16')
+        config |= ADS1x15_CONFIG_GAIN[gain]
+        # Set the mode (continuous or single shot).
+        config |= mode
+        # Get the default data rate if none is specified (default differs between
+        # ADS1015 and ADS1115).
+        if data_rate is None:
+            data_rate = self._data_rate_default()
+        # Set the data rate (this is controlled by the subclass as it differs
+        # between ADS1015 and ADS1115).
+        config |= self._data_rate_config(data_rate)
+        config |= ADS1x15_CONFIG_COMP_QUE_DISABLE  # Disble comparator mode.
+        # Send the config value to start the ADC conversion.
+        # Explicitly break the 16-bit value down to a big endian pair of bytes.
+        self._device.writeList(ADS1x15_POINTER_CONFIG, [(config >> 8) & 0xFF, config & 0xFF])
+        # Wait for the ADC sample to finish based on the sample rate plus a
+        # small offset to be sure (0.1 millisecond).
+        time.sleep(1.0/data_rate+0.0001)
+        # Retrieve the result.
+        result = self._device.readList(ADS1x15_POINTER_CONVERSION, 2)
+        return self._conversion_value(result[1], result[0])
+
+    # The rest of the class implementation remains unchanged.
+
+class ADS1115(ADS1x15):
+    """ADS1115 16-bit analog to digital converter instance."""
+
+    def __init__(self, *args, **kwargs):
+        super(ADS1115, self).__init__(*args, **kwargs)
+
+    def _data_rate_default(self):
+        # Default from datasheet page 16, config register DR bit default.
+        return 128
+
+    def _data_rate_config(self, data_rate):
+        if data_rate not in ADS1115_CONFIG_DR:
+            raise ValueError('Data rate must be one of: 8, 16, 32, 64, 128, 250, 475, 860')
+        return ADS1115_CONFIG_DR[data_rate]
+
+    def _conversion_value(self, low, high):
+        # Convert to 16-bit signed value.
+        value = ((high & 0xFF) << 8) | (low & 0xFF)
+        # Check for sign bit and turn into a negative value if set.
+        if value & 0x8000 != 0:
+            value -= 1 << 16
+        return value
+
+
+class ADS1015(ADS1x15):
+    """ADS1015 12-bit analog to digital converter instance."""
+
+    def __init__(self, *args, **kwargs):
+        super(ADS1015, self).__init__(*args, **kwargs)
+
+    def _data_rate_default(self):
+        # Default from datasheet page 19, config register DR bit default.
+        return 1600
+
+    def _data_rate_config(self, data_rate):
+        if data_rate not in ADS1015_CONFIG_DR:
+            raise ValueError('Data rate must be one of: 128, 250, 490, 920, 1600, 2400, 3300')
+        return ADS1015_CONFIG_DR[data_rate]
+
+    def _conversion_value(self, low, high):
+        # Convert to 12-bit signed value.
+        value = ((high & 0xFF) << 4) | ((low & 0xFF) >> 4)
+        # Check for sign bit and turn into a negative value if set.
+        if value & 0x800 != 0:
+            value -= 1 << 12
+        return value
